c17736a01f56458453319a3561250978caa538fe
Ha Nguyen Minh Hieu - 0942 <hanguyenminhhieu123@gmail.com>
2026-03-09 10:49:24 +0700

    up file main

---
+++ main.py
@@ -0,0 +1,7 @@
+#sum a-b
+def sum(a,b): return a+b
+#subtract a-b
+def subtract(a,b): return a-b
+if __name__ == "__main__":  
+    print("Sum of 5 and 3 is: ", sum(5,3))
+    print("Difference of 5 and 3 is: ", subtract(5,3))
